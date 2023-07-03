3e24a15fdf94b402d1bbf4daabc0a0adda3bc9fd
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-07-03 05:58:15 -0500

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,124 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div
+      id="app"
+      className="min-h-screen">
+      <div class="page brand-background min-h-screen">
+        <div
+          class="page__content"
+          id="page-ui-container">
+          <div
+            class="card card--no-padding"
+            data-id="card">
+            <div class="card__content">
+              <div class="flex-container">
+                <div class="branding-template-logo-container">
+                  <div class="org-logo">
+                    <img
+                      class="org-logo__image"
+                      src="https://d3uinntk0mqu3p.cloudfront.net/branding/market/a3d073bc-3108-49ad-b96c-404bea59a1d0.png"
+                      alt="Company Logo"
+                      title=""
+                      style={{}}
+                    />
+                  </div>
+                </div>
+                <div class="branding-template-form-container">
+                  <div>
+                    <div data-id="login-routes">
+                      <h1
+                        class="heading branding-template-heading sr-only"
+                        data-id="username-password-heading">
+                        Sign On
+                      </h1>
+                      <form
+                        class="form"
+                        data-id="username-password-validator-form">
+                        <div
+                          class="float-label placeholder-shown"
+                          data-id="username-input">
+                          <input
+                            class="text-input float-label__input branding-template-text-input"
+                            data-id="username-input-input"
+                            id="username"
+                            name="username"
+                            placeholder="Username"
+                            type="text"
+                            autocomplete="on"
+                            value=""
+                          />
+                          <label
+                            class="float-label__label"
+                            for="username">
+                            Username
+                          </label>
+                        </div>
+                        <div
+                          class="float-label placeholder-shown"
+                          data-id="password-input">
+                          <input
+                            class="text-input text-input--pasword float-label__input branding-template-password-input"
+                            data-id="password-input-input"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            type="password"
+                            autocomplete="on"
+                            value=""
+                          />
+                          <div
+                            aria-label="show password"
+                            aria-pressed="false"
+                            class="text-input__icon text-input__icon--hidden"
+                            role="button"
+                            tabindex="0"></div>
+                          <label
+                            class="float-label__label"
+                            for="password">
+                            Password
+                          </label>
+                        </div>
+                        <div>
+                          <button
+                            class="button file-input--button branding-template-primary-button button--primary brand-primary-bg"
+                            data-id="submit-button"
+                            type="submit">
+                            Sign On
+                          </button>
+                        </div>
+                        <div
+                          class="form"
+                          data-id="social-providers">
+                          <div> </div>
+                        </div>
+                        <div
+                          class="text-block branding-template-forgot-password branding-template-link-text"
+                          data-id="dettached-forgot-password">
+                          <div class="text-block--overflow-wrap">
+                            <a
+                              href="#"
+                              data-id="recovery-button"
+                              role="button">
+                              Forgot Password
+                            </a>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div
+                    data-id="footer-container"
+                    class="branding-template-footer-container"></div>
+                </div>
+              </div>
+
+              <div data-id="page-copyright"></div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
